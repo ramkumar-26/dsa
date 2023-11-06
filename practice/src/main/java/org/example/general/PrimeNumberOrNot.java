08fc6aa92e81d04fe0586e29787dbf5bca2e0eb6
ramkumar-26 <ramkumar.ds26@gmail.com>
2023-11-06 22:11:50 +0530

    isPrime [6th Nov 2023]

---
+++ practice/src/main/java/org/example/general/PrimeNumberOrNot.java
@@ -0,0 +1,32 @@
+package org.example.general;
+
+import java.util.Scanner;
+
+/*
+ * Date : 6 November 2023
+ * Level: Easy
+ * Link :
+ * Reference :
+ * Tag : #primeNumber  #whileLoop
+ * */
+public class PrimeNumberOrNot {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        System.out.println(isPrime(n));
+    }
+
+    private static boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        int c = 2;
+        while (c * c <= n) {
+            if (n % c == 0) {
+                return false;
+            }
+            c++;
+        }
+        return (c * c) > n;
+    }
+}
