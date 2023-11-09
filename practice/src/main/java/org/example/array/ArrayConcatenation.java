bc6535216f59b53f4fc83d60ea4c60c44ad7bd55
ramkumar-26 <ramkumar.ds26@gmail.com>
2023-11-09 19:06:16 +0530

    [9th Nov 2023]

---
+++ practice/src/main/java/org/example/array/ArrayConcatenation.java
@@ -0,0 +1,27 @@
+package org.example.array;
+
+/*
+ * Date : 9 November 2023
+ * Level: Easy
+ * Link :
+ * Reference : https://leetcode.com/problems/concatenation-of-array/
+ * Tag :
+ * */
+public class ArrayConcatenation {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 5};
+        getConcatenation(nums);
+    }
+    public static int[] getConcatenation(int[] nums) {
+        int[] ans = new int[nums.length * 2];
+        int index = 0;
+        for(int i=0;i<ans.length;i++){
+            if(index == nums.length){
+                index = 0;
+            }
+            ans[i] = nums[index++];
+        }
+        return ans;
+
+    }
+}
