08ed83ec4d899fc2869e2ae4e5fef9f6457d19fa
Ramkumar <ramkumar.ds26@gmail.com>
2023-11-13 22:48:18 +0530

    Update ArrayConcatenation.java

--- practice/src/main/java/org/example/array/ArrayConcatenation.java
+++ practice/src/main/java/org/example/array/ArrayConcatenation.java
@@ -9,7 +9,7 @@ package org.example.array;
  * */
 public class ArrayConcatenation {
     public static void main(String[] args) {
-        int[] nums = {1, 2, 3, 5};
+        int[] nums = {1, 2, 3, 4, 5};
         getConcatenation(nums);
     }
     public static int[] getConcatenation(int[] nums) {
